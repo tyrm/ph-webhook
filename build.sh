fddd3b67b4d264088283692a64e23dbacd4cfbee
Tyr Mactire <tyr@pettingzoo.co>
2018-11-21 23:25:23 -0800

    nothing is absolute

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 rm -Rf ph-warehouse
 
-/usr/local/go/bin/go get github.com/gobuffalo/packr
-/usr/local/go/bin/go get github.com/google/jsonapi
-/usr/local/go/bin/go get github.com/gorilla/mux
-/usr/local/go/bin/go get github.com/juju/loggo
-/usr/local/go/bin/go get github.com/lib/pq
-/usr/local/go/bin/go get github.com/rubenv/sql-migrate
+go get github.com/gobuffalo/packr
+go get github.com/google/jsonapi
+go get github.com/gorilla/mux
+go get github.com/juju/loggo
+go get github.com/lib/pq
+go get github.com/rubenv/sql-migrate
 
-CGO_ENABLED=0 GOOS=linux /usr/local/go/bin/go build -v -a -installsuffix cgo -o ph-warehouse .
+CGO_ENABLED=0 GOOS=linux go build -v -a -installsuffix cgo -o ph-warehouse .
